d01e7c0189cf59170811474b434ef2ded5839d10
jiacheliu3 <jiacheliu3@gmail.com>
2015-12-23 15:22:21 +0800

    3rd gogogo

--- JOS/grails-app/domain/jos/Customer.groovy
+++ JOS/grails-app/domain/jos/Customer.groovy
@@ -10,5 +10,14 @@ class Customer {
 	Set<House> following=new HashSet<>();
 	Set<Ticket> tickets=new HashSet<>();
 	static constraints = {
+		name(blank:false,nullable:false)
+		address(blank:true,nullable:true)
+		email(blank:true,nullable:true)
+		mobile(blank:true,nullable:true)
+	}
+	static mapping={
+		tickets sqlType: 'VARBINARY(10000)'
+		following sqlType: 'VARBINARY(10000)'
+
 	}
 }
--- JOS/grails-app/domain/jos/House.groovy
+++ JOS/grails-app/domain/jos/House.groovy
@@ -6,14 +6,25 @@ class House {
 	String address;
 	String tel;
 	String homepage;
-	
+
 	//relationship management
 	Map<String,Integer> nextTicketNumber=new HashMap<>();
 	Map<String,Integer> currentTicketNumber=new HashMap<>();
 	Set<Customer> customers=new HashSet<>();
 	Map<String,TreeSet<Ticket>> waitingList=new HashMap<>();
-    static constraints = {
-    }
-	
+	static constraints = {
+		name(blank:false,nullable:false)
+		address(blank:true,nullable:true)
+		homepage(blank:true,nullable:true)
+		tel(blank:true,nullable:true)
+		
+		
+	}
+	static mapping={
+		nextTicketNumber sqlType: 'VARBINARY(10000)'
+		currentTicketNumber sqlType: 'VARBINARY(10000)'
+		customers sqlType: 'VARBINARY(10000)'
+		waitingList sqlType: 'VARBINARY(10000)'
+	}
 	
 }
